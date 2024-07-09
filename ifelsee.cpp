7d2ec94b864adaecceec49393f71396031c3cf57
Koharu1265 <Sandeee1265@gmail.com>
2024-07-09 07:29:20 +0700

    Test

---
+++ ifelsee.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    float a,b,hasil;
+    char op;
+
+
+    cout << "selamat datang di kalkulator case ( kalkulator ini masih tahap beta )" <<endl;
+    cout << "silahkan masukan bilangan pertama = ";
+
+    cin >> a;
+
+    cout << "Masukan operasi hitung = ";
+    cin >> op;
+
+    cout << "Masukan bilangan kedua = ";
+    cin >> b;
+
+    switch (op) {
+        case '+' : 
+        hasil = a + b;
+
+        break;
+
+        case '-' :
+        hasil = a - b;
+        
+        break;
+
+        case 'x' :
+        hasil = a * b;
+
+        break;
+
+        case '/':
+        hasil = a/b;
+
+        break;
+
+        default:
+        cout << "invalid!!";
+    } 
+
+cout << "hasil dari perhitungan " << a << " " <<  op << " " << b << " " << "adalah = " << hasil;
+
+
+
+
+
+
+
+
+
+
+    
+}
